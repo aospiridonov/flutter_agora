ffbfe7ba0a8a1dfb969abda34853a17dee9a1c44
aospiridonov <aospiridonov@gmail.com>
2022-05-23 12:57:25 +0300

    make: index paga: cotroller, permission, agora roles

--- lib/pages/index.dart
+++ lib/pages/index.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:developer';
+
+import 'package:agora_rtc_engine/rtc_engine.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'call.dart';
 
 class IndexPage extends StatefulWidget {
   const IndexPage({Key? key}) : super(key: key);
@@ -8,12 +14,97 @@ class IndexPage extends StatefulWidget {
 }
 
 class _IndexPageState extends State<IndexPage> {
+  final _channelController = TextEditingController(text: 'flutter_agora');
+  bool _validateError = false;
+  ClientRole? _role = ClientRole.Broadcaster;
+
+  @override
+  void dispose() {
+    _channelController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-      title: const Text('Agora'),
-      centerTitle: true,
-    ));
+      appBar: AppBar(
+        title: const Text('Agora'),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            children: [
+              const SizedBox(height: 40),
+              Image.network(
+                  'https://d1nhio0ox7pgb.cloudfront.net/_img/g_collection_png/standard/512x512/user_mobile_phone.png'),
+              const SizedBox(height: 40),
+              TextField(
+                controller: _channelController,
+                decoration: InputDecoration(
+                    errorText:
+                        _validateError ? 'Channel name is mandatory' : null,
+                    border: const UnderlineInputBorder(
+                      borderSide: BorderSide(width: 1),
+                    ),
+                    hintText: 'Channel name'),
+              ),
+              RadioListTile(
+                title: const Text('Broadcaster'),
+                onChanged: (ClientRole? value) {
+                  setState(() {
+                    _role = value;
+                  });
+                },
+                value: ClientRole.Broadcaster,
+                groupValue: _role,
+              ),
+              RadioListTile(
+                title: const Text('Audience'),
+                onChanged: (ClientRole? value) {
+                  setState(() {
+                    _role = value;
+                  });
+                },
+                value: ClientRole.Audience,
+                groupValue: _role,
+              ),
+              ElevatedButton(
+                onPressed: onJoin,
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size(double.infinity, 40),
+                ),
+                child: const Text('Join'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<void> onJoin() async {
+    setState(() {
+      _validateError = _channelController.text.isEmpty ? true : false;
+    });
+    if (_channelController.text.isNotEmpty) {
+      await _handleCameraAndMic(Permission.camera);
+      await _handleCameraAndMic(Permission.microphone);
+      await Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => CallPage(
+            channelName: _channelController.text,
+            role: _role,
+          ),
+        ),
+      );
+    }
+  }
+
+  Future<void> _handleCameraAndMic(Permission permission) async {
+    final status = await permission.request();
+    log(status.toString());
   }
 }
