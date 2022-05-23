8105dd583aeecdf9d37a3c90af345cdc9cf97c70
aospiridonov <aospiridonov@gmail.com>
2022-05-23 11:47:40 +0300

    add: dependency in pubspec add: base widget and settings

---
+++ lib/pages/index.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class IndexPage extends StatefulWidget {
+  const IndexPage({Key? key}) : super(key: key);
+
+  @override
+  State<IndexPage> createState() => _IndexPageState();
+}
+
+class _IndexPageState extends State<IndexPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+      title: const Text('Agora'),
+      centerTitle: true,
+    ));
+  }
+}
